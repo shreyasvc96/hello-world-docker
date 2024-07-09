7db504ed08137484f2bf45f232467013711586ba
Shreyas Contractor <shreyas.contractor2@t-mobile.com>
2024-07-09 13:32:03 -0500

    Updating Print Message!

--- src/main/java/com/aws/microservice/TestController.java
+++ src/main/java/com/aws/microservice/TestController.java
@@ -10,7 +10,7 @@ public class TestController {
 
     @GetMapping("/")
     public String index(){
-        return "Hello World!";
+        return "Hello World!!!";
     }
 
     @GetMapping("/time")
